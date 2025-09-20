37fec842ad6cc6adfd113149fbaf9128d51005ca
gainax2k1 <gainax2k1@gmail.com>
2025-09-19 22:50:05 -0700

    ready to refactor

--- dist/command_help.js
+++ dist/command_help.js
@@ -1,5 +1,7 @@
 export function commandHelp(commands) {
-    console.log("Available commands:");
+    console.log("Welcome to the Pokedex!");
+    console.log("Usage:");
+    //console.log("Available commands:");
     // iterate over ALL commands in the registry (passed in)
     for (const [name, command] of Object.entries(commands)) {
         console.log(`${name}: ${command.description}`);
--- src/command_help.ts
+++ src/command_help.ts
@@ -1,7 +1,9 @@
 import { CLICommand } from "./command.js";
 
 export function commandHelp(commands: Record<string, CLICommand>) {
-    console.log("Available commands:");
+    console.log("Welcome to the Pokedex!");
+    console.log("Usage:")
+    //console.log("Available commands:");
     // iterate over ALL commands in the registry (passed in)
     for (const [name, command] of Object.entries(commands)) {
         console.log(`${name}: ${command.description}`);
